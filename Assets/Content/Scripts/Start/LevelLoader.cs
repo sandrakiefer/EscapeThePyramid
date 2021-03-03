7831d61ab35a4efadb9a709a777d704f1f9bcbd0
Sandra Kiefer <sandra.kiefer@student.hs-rm.de>
2021-03-04 00:26:47 +0100

    StartScreen

---
+++ Assets/Content/Scripts/Start/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace DE.HSRM.MI.EscapeThePyramid.Start
+{
+    public class LevelLoader : MonoBehaviour
+    {
+
+        public GameObject loadingScreen;
+        public Slider slider;
+        public Text progressText;
+
+        public void LoadLevel(int sceneIndex)
+        {
+            StartCoroutine(LoadAsynchronously(sceneIndex));
+        }
+
+        IEnumerator LoadAsynchronously(int sceneIndex)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+            loadingScreen.SetActive(true);
+
+            while (!operation.isDone)
+            {
+                float progress = Mathf.Clamp01(operation.progress / .9f);
+                slider.value = progress;
+                progressText.text = progress * 100f + " %";
+                yield return null;
+            }
+        }
+
+    }
+}
---
+++ Assets/Content/Scripts/Start/QuitApplicationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HSRM.Core.Commands;
+
+namespace DE.HSRM.MI.EscapeThePyramid.Start
+{
+    public class QuitApplicationController : BaseCommand
+    {
+        protected override void ExecuteCommand()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+
+}
